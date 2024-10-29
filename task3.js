2fc6df408ef881effcd889a3c2557bab0674f308
sumit meghrajani <sumit.disolutions@gmail.com>
2024-10-29 17:41:02 +0530

    first commit

---
+++ task3.js
@@ -0,0 +1,25 @@
+function task3(ind){
+switch(ind){
+   case 0:
+   document.getElementById('resultclr').style.backgroundColor = "red";
+   break;
+   case 1:
+   document.getElementById('resultclr').style.backgroundColor = "blue";
+   break;
+   case 2:
+   document.getElementById('resultclr').style.backgroundColor = "green";
+   break;
+   case 3:
+   document.getElementById('resultclr').style.backgroundColor = "yellow";
+   break;
+   case 4:
+   document.getElementById('resultclr').style.backgroundColor = "black";
+   break;
+   case 5:
+    document.getElementById('resultclr').style.backgroundColor = "skyblue";
+    break;
+   default:
+    document.getElementById('resultclr').style.backgroundColor = "brown";
+    break;
+}
+}task3()
